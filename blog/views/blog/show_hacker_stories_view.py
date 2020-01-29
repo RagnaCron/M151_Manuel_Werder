2e71695053060dd3f5774d980668bdfbb9427766
Manuel <Werder>
2020-01-29 13:20:32 +0100

    basic read blog implemented

--- blog/views/blog/show_hacker_stories_view.py
+++ blog/views/blog/show_hacker_stories_view.py
@@ -1,30 +1,13 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from ...editor_forms import HackerStory
 from ...models import StoryModel
 
 
-class HackerStoriesListView(ListView):
-	template_name = 'blog/show_hacker_stories.html'
-	model = StoryModel
-	context_object_name = 'stories'
-	paginate_by = 10
-
-
 def show_hacker_stories_view(request):
+	queryset = StoryModel.objects.all()
+	context = {'stories': queryset}
+	return render(request, 'blog/show_hacker_stories.html', context)
 
-	# if request.method == "POST":
-	# 	edit_story_form = HackerStory(request.POST)
-	# 	if edit_story_form.is_valid():
-	# 		story = edit_story_form.save(commit=False)
-	# 		story.user_fk = request.user
-	# 		story.save()
-	# 		error = edit_story_form.errors
-	# 	else:
-	# 		error = edit_story_form.errors
-	# else:
-	# 	edit_story_form = HackerStory()
-	# 	error = edit_story_form.errors
-	# context = {'new_story_form': edit_story_form, 'error': error}
-	# return render(request, 'blog/new_hacker_story.html', context=context)
-	return render(request, 'blog/show_hacker_stories.html')
+
+def show_story_view(request, story_title):
+	story = StoryModel.objects.get(story_title=story_title)
+	return render(request, 'blog/show_hacker_story.html', context={'story': story})
