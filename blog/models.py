96032a99d023e6568f05a1d587b91053efea3273
Manuel <Werder>
2020-01-29 13:37:30 +0100

    added creation date to StoryModel

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,6 @@ from froala_editor.fields import FroalaField
 # Create your models here.
 class StoryModel(models.Model):
 	user_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+	creation_date = models.DateTimeField(auto_now=True)
 	story_title = models.CharField(unique=True, max_length=50)
 	story_content = FroalaField()
-# 	ToDo: Creation DATE
