3de471d85f677bc46d458a7e4ee6fc1abb750254
Manuel <Werder>
2020-01-31 00:52:51 +0100

    some style changes

--- hacker/settings.py
+++ hacker/settings.py
@@ -79,14 +79,8 @@ DB_NAME = 'db'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get(DB_NAME, 'db'),
-        # 'USER': 'master',
-        # 'PASSWORD': 'kloxet82',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
     }
 }
 
